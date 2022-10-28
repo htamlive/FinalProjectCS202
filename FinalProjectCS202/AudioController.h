4706e70572ab2097d53c913b2984fdd48bb05ad5
ltp1405 <lamtungphan1405@gmail.com>
2022-10-28 17:06:14 +0700

    Minimize interface exposure

--- FinalProjectCS202/AudioController.h
+++ FinalProjectCS202/AudioController.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <SFML/Audio.hpp>
 #include <SFML/Audio/Music.hpp>
 #include <SFML/Audio/Sound.hpp>
@@ -18,7 +20,16 @@ namespace Music
     enum ID { Game, Menu, Pause };
 }
 
+/**
+ * A Singleton unit used for controling audio
+ * Only classes that inherits SoundPlayable will be able to access audio play interface
+*/
 class AudioController {
+    AudioController();
+    static AudioController* _instance;
+
+    friend class SoundPlayable;
+
     std::map<Music::ID, std::string> musicPlaylist;
     std::map<SoundEffect::ID, std::unique_ptr<sf::SoundBuffer>> soundBar;
 
@@ -27,13 +38,11 @@ class AudioController {
     float musicVolume;
     bool isMuted;
 
-    AudioController();
-    static AudioController* _instance;
-
     void loadMusicFromFile(Music::ID type, const std::string &path);
     void loadSoundFromFile(SoundEffect::ID id, const std::string &path);
     sf::SoundBuffer& getSoundBuffer(SoundEffect::ID id);
     void updateSettings();
+    void playSound(SoundEffect::ID id);
 
 public:
     AudioController(const AudioController&) = delete;
@@ -43,7 +52,6 @@ public:
     void playMusic(Music::ID id);
     void resumeMusic();
     void pauseMusic();
-    void playSound(SoundEffect::ID id);
     void setMuted(bool);
     void setMusicVolumn(float vol);
 };
