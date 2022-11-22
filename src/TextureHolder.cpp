f6754052d2c793bde9ac761f5ffbe1b26d859d21
ltp1405 <LamTungPro1405@gmail.com>
2022-11-22 15:06:08 +0700

    🔨 Improve error handling and docstring.

--- src/TextureHolder.cpp
+++ src/TextureHolder.cpp
@@ -1,6 +1,7 @@
 #include <SFML/Graphics/Texture.hpp>
 #include <iostream>
 #include <memory>
+#include <cassert>
 
 #include "TextureHolder.h"
 
@@ -23,15 +24,24 @@ void TextureHolder::load(Texture::ID type, const std::string &filename,
             std::move(texture), spriteCount, spriteSize, textureRow});
     } else {
         std::cerr << "Loading texture from \"" << filename << "\" failed.\n";
+        assert(0);
     }
 }
 
 sf::Texture const &TextureHolder::getTexture(Texture::ID id) const {
     auto found = textures.find(id);
+    if (found == textures.end()) {
+        std::cerr << "TextureHolder::getTexture: Texture not found.\n";
+        assert(0);
+    }
     return found->second.get()->texture;
 }
 
 SpriteSheet const &TextureHolder::get(Texture::ID id) const {
     auto found = textures.find(id);
+    if (found == textures.end()) {
+        std::cerr << "TextureHolder::get: Texture not found.\n";
+        assert(0);
+    }
     return *found->second;
 }
