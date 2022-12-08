9507de6c0dc0c0530976a5a8415ea9afdaea6b18
ltp1405 <lamtungphan1405@gmail.com>
2022-12-08 23:48:29 +0700

    Add new class for stateless object

---
+++ src/SpriteNode.cpp
@@ -0,0 +1,14 @@
+#include "SpriteNode.h"
+
+SpriteNode::SpriteNode(Texture::ID id, sf::Vector2f size) {
+    sprite.setTexture(TextureHolder::instance().get(id).texture);
+    sprite.setScale(size.x / sprite.getLocalBounds().width, size.y / sprite.getLocalBounds().height);
+}
+
+void SpriteNode::drawCurrent(sf::RenderTarget& target, sf::RenderStates states) const {
+    target.draw(sprite, states);
+}
+
+void SpriteNode::updateCurrent(sf::Time dt) {
+    // Do nothing
+}
