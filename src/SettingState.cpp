b957a400dcfb74dd0fbed40831fd03b7a3367b16
htamlive <nghichmaychoi@gmail.com>
2022-12-12 18:17:30 +0700

    add slider

--- src/SettingState.cpp
+++ src/SettingState.cpp
@@ -10,6 +10,16 @@ SettingState::SettingState(sf::RenderWindow* window, std::vector<State*>* states
 	//initSearchBar();
 	initButtons();
 
+	this->gui->get<tgui::Slider>("sliderMusic")->onValueChange([&]() {
+		int val = this->gui->get<tgui::Slider>("sliderMusic")->getValue();
+		this->gui->get<tgui::EditBox>("eBoxMusic")->setText(to_string(val));
+		});
+
+	this->gui->get<tgui::Slider>("sliderSound")->onValueChange([&]() {
+		int val = this->gui->get<tgui::Slider>("sliderSound")->getValue();
+		this->gui->get<tgui::EditBox>("eBoxSound")->setText(to_string(val));
+		});
+
 }
 
 void SettingState::render(sf::RenderTarget* target)
