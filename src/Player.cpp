5bbfac9748bc33eed50f6fc0347b92c18ff92825
ltp1405 <lamtungphan1405@gmail.com>
2022-12-14 09:05:15 +0700

    Refactor Player to State Pattern

--- src/Player.cpp
+++ src/Player.cpp
@@ -1,137 +1,91 @@
 #include "Player.h"
 #include "Consts.h"
-#include <iostream>
+#include "Enums.h"
+#include "PlayerState.h"
+#include "SceneNode.h"
 #include <SFML/Graphics/Sprite.hpp>
 #include <SFML/System/Time.hpp>
+#include <iostream>
+#include <memory>
 
 void Player::updateCurrent(sf::Time dt) {
-    if (animation.isFinished()) {
-        onJumpAnimationFinished();
-    }
-
-    switch (state) {
-    case Idle:
-        break;
-    case Jumping:
-        updateJump(dt);
-        break;
-    case Colliding:
-        updateCollision(dt);
-        break;
-    }
-
+    state->update(dt);
     Entity::updateCurrent(dt);
 }
 
-Player::Player() : jumpTexture(Texture::ID::PlayerJumpUp), idleTexture(Texture::ID::PlayerIdleUp) {
+Player::Player()
+    : jumpTexture(Texture::ID::PlayerJumpUp),
+      idleTexture(Texture::ID::PlayerIdleUp), state(new IdleState(this)) {
     staticPos = getPosition();
+    setState(new IdleState(this));
     animation = AnimationMachine(idleTexture, sf::seconds(5), true);
 }
 
 Player::Player(sf::Vector2f position, sf::Vector2f size)
-        : Entity(Texture::ID::PlayerIdleUp, position, size, DEF_PLAYER_VELOCITY),
-          jumpTexture(Texture::ID::PlayerJumpUp),
-          idleTexture(Texture::ID::PlayerIdleUp) {
+    : Entity(Texture::ID::PlayerIdleUp, position, size, DEF_PLAYER_VELOCITY),
+      jumpTexture(Texture::ID::PlayerJumpUp),
+      idleTexture(Texture::ID::PlayerIdleUp), state(new IdleState(this)) {
+    setState(new IdleState(this));
     staticPos = getPosition();
     setVelocity({0, 0});
     adjustBounds(20, 20, 40, 40);
 }
 
 void Player::onKeyPressed(sf::Event::KeyEvent event) {
-    if (state != Idle) {
+    if (!dynamic_cast<IdleState *>(state)) {
         return;
     }
     auto newPos = staticPos;
-    if (state == Idle) {
+    if (dynamic_cast<IdleState *>(state)) {
         preJumpPos = getPosition();
         switch (event.code) {
-            case sf::Keyboard::W:
-            case sf::Keyboard::Up:
-                newPos = {staticPos.x, staticPos.y - GRID_SIZE.y};
-                jumpTexture = Texture::ID::PlayerJumpUp;
-                idleTexture = Texture::ID::PlayerIdleUp;
-                break;
-            case sf::Keyboard::S:
-            case sf::Keyboard::Down:
-                newPos = {staticPos.x, staticPos.y + GRID_SIZE.y};
-                jumpTexture = Texture::ID::PlayerJumpDown;
-                idleTexture = Texture::ID::PlayerIdleDown;
-                break;
-            case sf::Keyboard::A:
-            case sf::Keyboard::Left:
-                newPos = {staticPos.x - GRID_SIZE.x, staticPos.y};
-                jumpTexture = Texture::ID::PlayerJumpLeft;
-                idleTexture = Texture::ID::PlayerIdleLeft;
-                break;
-            case sf::Keyboard::D:
-            case sf::Keyboard::Right:
-                newPos = {staticPos.x + GRID_SIZE.x, staticPos.y};
-                jumpTexture = Texture::ID::PlayerJumpRight;
-                idleTexture = Texture::ID::PlayerIdleRight;
-                break;
-            default:
+        case sf::Keyboard::W:
+        case sf::Keyboard::Up:
+            newPos = {staticPos.x, staticPos.y - GRID_SIZE.y};
+            jumpTexture = Texture::ID::PlayerJumpUp;
+            idleTexture = Texture::ID::PlayerIdleUp;
+            break;
+        case sf::Keyboard::S:
+        case sf::Keyboard::Down:
+            newPos = {staticPos.x, staticPos.y + GRID_SIZE.y};
+            jumpTexture = Texture::ID::PlayerJumpDown;
+            idleTexture = Texture::ID::PlayerIdleDown;
+            break;
+        case sf::Keyboard::A:
+        case sf::Keyboard::Left:
+            newPos = {staticPos.x - GRID_SIZE.x, staticPos.y};
+            jumpTexture = Texture::ID::PlayerJumpLeft;
+            idleTexture = Texture::ID::PlayerIdleLeft;
+            break;
+        case sf::Keyboard::D:
+        case sf::Keyboard::Right:
+            newPos = {staticPos.x + GRID_SIZE.x, staticPos.y};
+            jumpTexture = Texture::ID::PlayerJumpRight;
+            idleTexture = Texture::ID::PlayerIdleRight;
+            break;
+        default:
 
-                break;
+            break;
         }
-        animation = AnimationMachine(jumpTexture, JUMP_DURATION, false);
         staticPos = newPos;
-        timeJumped = sf::Time::Zero;
-        state = Jumping;
+        setState(new JumpingState(this));
     }
 }
 
-bool Player::isJumping() const { return timeJumped < JUMP_DURATION; }
-
-void Player::updateJump(sf::Time dt) {
-    auto length = staticPos - getPosition();
-    if (isJumping()) {
-        auto timeLeft = JUMP_DURATION - timeJumped;
-        setVelocity({length.x / timeLeft.asSeconds(), length.y / timeLeft.asSeconds()});
-        // The scale to ease the jumping movement
-        // Derived from the formula: y = 1 - (x - 1)^2
-        float scale = -2 * ((timeJumped.asSeconds() / JUMP_DURATION.asSeconds()) - 1);
-        setVelocity(getVelocity() * scale);
-        timeJumped += dt;
-    } else {
-        setPosition(staticPos);
-        setVelocity({0, 0});
-        state = Idle;
-    }
-}
-
-void Player::onJumpAnimationFinished() {
-    animation = AnimationMachine(idleTexture, DEF_ANIMATION_DURATION, true);
-}
-
-void Player::onCollision(Entity *other) {
-    if (state == Idle) {
+void Player::onCollision(SceneNode *other) {
+    if (dynamic_cast<IdleState *>(state)) {
         return;
     }
-    if (state == Jumping) {
-        state = Colliding;
-        collisionTime = sf::Time::Zero;
+    if (dynamic_cast<JumpingState *>(state)) {
+        setState(new CollidingState(this));
         setVelocity({0, 0});
         staticPos = preJumpPos;
     }
 }
 
-void Player::updateCollision(sf::Time dt) {
-    auto dist = staticPos - getPosition();
-    if (collisionTime < sf::seconds(0.3)) {
-        auto timeLeft = JUMP_DURATION - collisionTime;
-        setVelocity({dist.x / timeLeft.asSeconds(), dist.y / timeLeft.asSeconds()});
-        // The scale to ease the jumping movement
-        // Derived from the formula: y = 1 - (x - 1)^2
-        float scale = -2 * ((timeJumped.asSeconds() / JUMP_DURATION.asSeconds()) - 1);
-        setVelocity(getVelocity() * scale);
-        collisionTime += dt;
-    } else {
-        setPosition(preJumpPos);
-        setVelocity({0, 0});
-        state = Idle;
-    }
-}
+Category::Type Player::getCategory() const { return Category::Player; }
 
-Category Player::getCategory() const {
-    return Category::Player;
+void Player::setState(PlayerState *newState) {
+    delete state;
+    state = newState;
 }
--- src/Player.h
+++ src/Player.h
@@ -1,27 +1,48 @@
 #pragma once
 
 #include "Entity.h"
+#include "Enums.h"
+#include "SceneNode.h"
+#include <SFML/Graphics/Rect.hpp>
+#include "Effect.h"
 
-enum PlayerState {
-    Idle,
-    Jumping,
-    Colliding
-};
+// [ ] Boost tăng tốc
+// [ ] Boost small size
+// [ ] Mud
+// [ ] Rocket
+// [ ] Bomb
+// [ ] Cave // Chuột chũi -> Subway -> cá sấu dưới cống, Thang máy
+// [ ] Cổng điện
+// [ ] Cây cầu
+// [ ] Trộm chó (trúng là chết luôn)
+// [ ] Akshan
+// [ ] Dải caro kết thúc màn
+// [ ] Bãy caitlyn + R caitlyn
+// [ ] Bay lên vũ trụ + điều khiển ufo
 
 /**
  * @brief The Player class
  *
  * Note: character jumps only once each press. That allows user to control the speed (by pressing faster or slower).
  */
+
+class PlayerState;
 class Player : public Entity {
+    friend class PlayerState;
+    friend class JumpingState;
+    friend class IdleState;
+    friend class SmallSizeBoostingState;
+    friend class SpeedBoostingState;
+    friend class CollidingState;
 public:
     Player();
-
     Player(sf::Vector2f position, sf::Vector2f size);
 
     void onKeyPressed(sf::Event::KeyEvent);
-    void onCollision(Entity *other);
-    Category getCategory() const override;
+    void onCollision(SceneNode *other);
+    Category::Type getCategory() const override;
+    void setState(PlayerState *newState);
+    void takeDamage(int damage);
 
 private:
     void updateCurrent(sf::Time dt) override;
@@ -32,15 +53,11 @@ private:
 
     void onJumpAnimationFinished();
 
-    void updateCollision(sf::Time dt);
-
-    sf::Vector2f staticPos;
-
-    sf::Time timeJumped;
-
+    PlayerState *state;
     Texture::ID jumpTexture, idleTexture;
-    
-    sf::Time collisionTime;
+    sf::Vector2f staticPos;
     sf::Vector2f preJumpPos;
-    PlayerState state = Idle;
+    int health = 3;
+    bool onSizeBoost = false;
+    bool onSpeedBoost = false;
 };
